366319e33f593446a800552992d80ebe1abf0acd
oakgul <orhan.akgull@gmail.com>
2020-02-20 15:14:58 +0300

    Scoreboard is working

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <Head />
     <hr>  
     <Minefield />
+    <Scoreboard />
   </div>
 </template>
 
 <script>
 import Head from './components/Head.vue';
 import Minefield from './components/Minefield';
+import Scoreboard from './components/Scoreboard';
 
 export default {
   name: 'App',
   components: {
     Head,
     Minefield,
+    Scoreboard,
   }
 }
 </script>
--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        scoreboard comp..
+    <div class="container">
+
+        <div class="scoreboard float-right">
+            <div class="header">Top 10 Score</div>
+            <ul>
+                <li style="list-style-type:none" v-for="player in names" :key="player.id">  {{ player }}</li>
+            </ul>
+        </div>
+
     </div>
 </template>
+
+<script>
+export default {
+    computed : {
+        names() {
+            return this.$store.state.names;
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+    .header {
+        width: 100%;
+        height: 50px;
+        background-color: lightgreen;
+        text-align: center;
+        font-size: 30px;
+    }
+
+    .scoreboard {
+        width: 432px;
+        height: 330px;
+        background-color: lightslategray;
+        margin-top: -320px;
+        border: 1px solid black;
+    }
+    
+
+</style>
